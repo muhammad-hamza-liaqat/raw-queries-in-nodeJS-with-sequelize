fb728686b1fa6e22e9355a32460cfcfc19dfc35c
Muhammad Hamza <mh408800@gmail.com>
2023-12-06 18:02:42 +0500

    pino added

--- middleware/responseAPI.js
+++ middleware/responseAPI.js
@@ -1,40 +1,38 @@
 // genericResponseMiddleware.js
 
 const genericResponseMiddleware = (req, res, next) => {
-    res.sendApiResponse = (data, message = 'Success', statusCode = 200) => {
-      res.status(statusCode).json({
-        success: true,
-        statusCode,
-        data,
-      });
-    };
-  
-    res.sendApiError = (error, statusCode = 500) => {
-      const defaultMessage = 'Internal Server Error';
-  
-      if (typeof error === 'string') {
-        res.status(statusCode).json({
-          success: false,
-          message: error || defaultMessage,
-        });
-      } else if (error instanceof Error) {
-        console.error(error.stack);
-        res.status(statusCode).json({
-          success: false,
-          statusCode,
-          message: error.message || defaultMessage,
-        });
-      } else {
-        res.status(statusCode).json({
-          success: false,
-          message: defaultMessage,
-        });
-      }
-    };
-  
-    next();
+  res.sendApiResponse = (data, message = "Success", statusCode = 200) => {
+    res.status(statusCode).json({
+      success: true,
+      statusCode,
+      data,
+    });
   };
-  
-  module.exports = genericResponseMiddleware;
 
-  
+  res.sendApiError = (error, statusCode = 500) => {
+    const defaultMessage = "Internal Server Error";
+
+    if (typeof error === "string") {
+      res.status(statusCode).json({
+        success: false,
+        message: error || defaultMessage,
+      });
+    } else if (error instanceof Error) {
+      console.error(error.stack);
+      res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message: error.message || defaultMessage,
+      });
+    } else {
+      res.status(statusCode).json({
+        success: false,
+        message: defaultMessage,
+      });
+    }
+  };
+
+  next();
+};
+
+module.exports = genericResponseMiddleware;
